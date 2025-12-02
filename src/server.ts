abfc42a5b784d71a4e246d95f59f53f47d3bb9f2
asifahmed1810 <asifahmed18oct@gmail.com>
2025-12-02 23:36:33 +0600

    Added config folder

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,15 @@
 import express, { Request, Response } from "express"
-import {Pool} from "pg"
-import dotenv from "dotenv"
-import path from "path"
 
-dotenv.config({path: path.join(process.cwd(), ".env")})
+import config from "./config"
+import initDB, { pool } from "./config/db";
+
 
 const app = express()
-const port = 5000;
+const port = config.port; 
 
 app.use(express.json())
 
-const pool=new Pool({
-  connectionString: `${process.env.CONNECTION_STR}`
-})
-
-
-const initDB=async()=>{
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    age INT,
-    phone VARCHAR(15),
-    address TEXT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-    )
-    `);
-
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS todos(
-      id SERIAL PRIMARY KEY,
-      user_id INT REFERENCES users(id) ON DELETE CASCADE,
-      title VARCHAR(200) NOT NULL,
-      description TEXT,
-      completed BOOLEAN DEFAULT false,
-      due_date DATE,
-      created_at TIMESTAMP DEFAULT NOW(),
-      updated_at TIMESTAMP DEFAULT NOW()
-      )
-      `);
-
-
-}
-initDB()
+initDB();
 
 
 
@@ -130,6 +95,62 @@ app.get("/users/:id", async(req:Request , res:Response)=>{
     
   }
 })
+
+
+
+//todos
+
+app.post("/todos",async(req:Request, res:Response)=>{
+  
+  const {user_id, title}=req.body
+
+  try {
+
+    const result=await pool.query(`INSERT INTO todos(user_id , title) VALUES($1,$2) RETURNING *`, [user_id,title])
+
+
+    
+     res.status(201).json({
+    success:true,
+    message:"todo is posting successfully",
+    data:result.rows[0]
+  })
+
+  } catch (error:any) {
+     res.status(500).json({
+    success:false,
+    message:error.message,
+    
+  })
+
+  }
+
+ 
+})
+
+
+app.get("/todos",async(req:Request , res:Response)=>{
+  try {
+    const result =await pool.query(`SELECT * FROM todos `)
+
+    res.status(201).json({
+      success:true,
+      message:"Data retrived successfully",
+      data:result.rows
+    })
+    
+  } catch (err:any) {
+    res.status(500).json({
+      success:false,
+      message:err.message
+    })
+    
+  }
+})
+
+
+
+
 
 
 app.listen(port, () => {
